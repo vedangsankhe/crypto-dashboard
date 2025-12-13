7f8c2f0276801714305ce796259783c21fd7aafc
Vedang Sankhe <vedangsankhe@gmail.com>
2025-12-14 01:57:08 +0530

    navbar component added globally & imports

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Router, RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ RouterLinkActive, RouterLink ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
