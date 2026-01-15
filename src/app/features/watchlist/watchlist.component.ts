1ef02ef4b992283259ea062607ae8ad41af47ae0
Vedang Sankhe <vedangsankhe@gmail.com>
2026-01-16 01:57:26 +0530

    watchlist page working added

--- src/app/features/watchlist/watchlist.component.ts
+++ src/app/features/watchlist/watchlist.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WatchlistService } from '../../core/services/watchlist.service';
 
 @Component({
   selector: 'app-watchlist',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './watchlist.component.html',
   styleUrl: './watchlist.component.scss'
 })
 export class WatchlistComponent {
+
+  coins: any[] = [];
+
+  constructor(private watchlistService: WatchlistService) {}
+
+  ngOnInit(){
+    this.loadWatchlist()
+  }
+
+  loadWatchlist(){
+    this.coins = this.watchlistService.getWatchlist();
+  }
+
+  remove(id: string){
+    this.watchlistService.removeFromWatchlist(id);
+    this.loadWatchlist();
+  }
 
 }
