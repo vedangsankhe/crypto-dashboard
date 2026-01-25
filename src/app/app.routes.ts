7724b8926f8875ccbeb47a8dd63770a023616103
Vedang Sankhe <vedangsankhe@gmail.com>
2026-01-25 22:00:51 +0530

    implemented lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,28 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
-import { WatchlistComponent } from './features/watchlist/watchlist.component';
 import { watchlistGuard } from './core/guards/watchlist.guard';
 
 export const routes: Routes = [
     {
         path: '',
-        component: DashboardComponent
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () =>
+            import('./features/dashboard/dashboard.component')
+                .then(m => m.DashboardComponent)
     },
     {
         path: 'watchlist',
-        component: WatchlistComponent,
-        canActivate: [watchlistGuard]
+        canActivate: [watchlistGuard],
+        loadComponent: () =>
+            import('./features/watchlist/watchlist.component')
+                .then(m => m.WatchlistComponent)
+    },
+
+    {
+        path: '**',
+        redirectTo: 'dashboard'
     }
 ];
