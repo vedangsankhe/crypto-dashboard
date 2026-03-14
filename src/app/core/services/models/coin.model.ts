bab760bd5491db528d36088fc19f392712ceab7e
Vedang Sankhe <vedangsankhe@gmail.com>
2026-03-14 13:48:25 +0530

    coin changes for icon import

--- src/app/core/services/models/coin.model.ts
+++ src/app/core/services/models/coin.model.ts
@@ -1,8 +1,13 @@
-export interface coin{
-    id: string;
-    name: string;
-    symbol: string;
-    current_price: number;
-    marker_cap: number;
-    price_change_percentage_24h: number;
+export interface coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  price_change_percentage_24h: number;
+  sparkline_in_7d: {
+    price: number[];
+  };
 }
